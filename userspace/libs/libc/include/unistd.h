dd314670a55ccaa0da6e5fa3e86d7449aefe629b
Mr.UNIX <ibrahimkaikaa@gmail.com>
2025-11-29 21:07:08 +0100

    Implement an initial userspace libc

---
+++ userspace/libs/libc/include/unistd.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2025, Ibrahim KAIKAA <ibrahimkaikaa@gmail.com>
+ * SPDX-License-Identifier: GPL-3.0
+ */
+
+static inline long syscall0(long num)
+{
+    long ret;
+    __asm__ volatile("int $0x80" : "=a"(ret) : "a"(num) : "rcx", "r11", "memory");
+    return ret;
+}
