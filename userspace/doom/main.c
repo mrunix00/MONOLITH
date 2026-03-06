aeb9cc6462497edc42f115fabd52833a93bb9345
Mr.UNIX <ibrahimkaikaa@gmail.com>
2026-03-06 10:49:28 +0100

    Port Doom

---
+++ userspace/doom/main.c
@@ -0,0 +1,605 @@
+/*
+ * Copyright (c) 2025, Ibrahim KAIKAA
+ * SPDX-License-Identifier: GPL-3.0
+ */
+
+#define DOOM_IMPLEMENT_MALLOC
+#define DOOM_IMPLEMENTATION
+#include <PureDOOM.h>
+
+#include <libdesktop.h>
+#include <libgfx.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+typedef struct
+{
+    int fd;
+    uint64_t size;
+    uint64_t pos;
+    bool writable;
+    bool size_known;
+} doom_file_t;
+
+static void doom_print_override(const char *str)
+{
+    (void) str;
+}
+
+static void *doom_malloc_override(int size)
+{
+    return malloc((size_t) size);
+}
+
+static void doom_free_override(void *ptr)
+{
+    free(ptr);
+}
+
+static bool mode_has_plus(const char *mode)
+{
+    if (!mode)
+        return false;
+    for (const char *p = mode; *p; ++p) {
+        if (*p == '+')
+            return true;
+    }
+    return false;
+}
+
+static void *doom_open_override(const char *filename, const char *mode)
+{
+    if (!filename || !mode)
+        return NULL;
+
+    int flags = 0;
+    bool writable = false;
+
+    if (mode[0] == 'r') {
+        flags = O_RDONLY;
+        writable = false;
+        if (mode_has_plus(mode)) {
+            flags = O_RDWR;
+            writable = true;
+        }
+    } else if (mode[0] == 'w') {
+        flags = O_WRONLY | O_CREAT | O_TRUNC;
+        writable = true;
+        if (mode_has_plus(mode)) {
+            flags = O_RDWR | O_CREAT | O_TRUNC;
+            writable = true;
+        }
+    } else if (mode[0] == 'a') {
+        flags = O_WRONLY | O_CREAT;
+        writable = true;
+        if (mode_has_plus(mode)) {
+            flags = O_RDWR | O_CREAT;
+            writable = true;
+        }
+    } else {
+        flags = O_RDONLY;
+    }
+
+    int fd = open(filename, flags);
+    if (fd < 0)
+        return NULL;
+
+    doom_file_t *file = (doom_file_t *) malloc(sizeof(doom_file_t));
+    if (!file) {
+        close(fd);
+        return NULL;
+    }
+
+    memset(file, 0, sizeof(*file));
+    file->fd = fd;
+    file->writable = writable;
+
+    file_stats_t stats;
+    if (fstat(fd, &stats) == 0 && stats.type == TYPE_FILE) {
+        file->size = stats.size;
+        file->size_known = true;
+    } else {
+        file->size = 0;
+        file->size_known = false;
+    }
+
+    file->pos = 0;
+
+    if (mode[0] == 'a') {
+        uint64_t end = file->size;
+        if (end > UINT32_MAX)
+            end = UINT32_MAX;
+        lseek(fd, (uint32_t) end, SEEK_SET);
+        file->pos = end;
+    }
+
+    return file;
+}
+
+static void doom_close_override(void *handle)
+{
+    if (!handle)
+        return;
+
+    doom_file_t *file = (doom_file_t *) handle;
+    close(file->fd);
+    free(file);
+}
+
+static int doom_read_override(void *handle, void *buf, int count)
+{
+    if (!handle || !buf || count <= 0)
+        return 0;
+
+    doom_file_t *file = (doom_file_t *) handle;
+    int read_bytes = read(file->fd, buf, (uint32_t) count);
+    if (read_bytes > 0) {
+        file->pos += (uint64_t) read_bytes;
+    } else if (read_bytes == 0 && !file->size_known) {
+        file->size = file->pos;
+        file->size_known = true;
+    }
+    return read_bytes;
+}
+
+static int doom_write_override(void *handle, const void *buf, int count)
+{
+    if (!handle || !buf || count <= 0)
+        return 0;
+
+    doom_file_t *file = (doom_file_t *) handle;
+    if (!file->writable)
+        return 0;
+
+    int written = write(file->fd, buf, (uint32_t) count);
+    if (written > 0) {
+        file->pos += (uint64_t) written;
+        if (file->pos > file->size)
+            file->size = file->pos;
+        file->size_known = true;
+    }
+    return written;
+}
+
+static int doom_seek_override(void *handle, int offset, doom_seek_t origin)
+{
+    if (!handle)
+        return -1;
+
+    doom_file_t *file = (doom_file_t *) handle;
+    int64_t base = 0;
+
+    switch (origin) {
+    case DOOM_SEEK_CUR:
+        base = (int64_t) file->pos;
+        break;
+    case DOOM_SEEK_END:
+        base = (int64_t) file->size;
+        break;
+    case DOOM_SEEK_SET:
+    default:
+        base = 0;
+        break;
+    }
+
+    int64_t desired = base + (int64_t) offset;
+    if (desired < 0)
+        desired = 0;
+    if (desired > (int64_t) UINT32_MAX)
+        desired = (int64_t) UINT32_MAX;
+
+    if (lseek(file->fd, (uint32_t) desired, SEEK_SET) < 0)
+        return -1;
+
+    file->pos = (uint64_t) desired;
+    return 0;
+}
+
+static int doom_tell_override(void *handle)
+{
+    if (!handle)
+        return -1;
+
+    doom_file_t *file = (doom_file_t *) handle;
+    if (file->pos > (uint64_t) INT32_MAX)
+        return (int) INT32_MAX;
+    return (int) file->pos;
+}
+
+static int doom_eof_override(void *handle)
+{
+    if (!handle)
+        return 1;
+
+    doom_file_t *file = (doom_file_t *) handle;
+    if (!file->size_known)
+        return 0;
+    return file->pos >= file->size;
+}
+
+static void doom_gettime_override(int *sec, int *usec)
+{
+    uint64_t ticks = get_ticks();
+    if (sec)
+        *sec = (int) (ticks / 1000);
+    if (usec)
+        *usec = (int) ((ticks % 1000) * 1000);
+}
+
+static void doom_exit_override(int code)
+{
+    exit();
+}
+
+static char *doom_getenv_override(const char *var)
+{
+    if (!var)
+        return NULL;
+    if (strcmp(var, "DOOMWADDIR") == 0)
+        return "system:/assets";
+    if (strcmp(var, "HOME") == 0)
+        return "system:/";
+    return NULL;
+}
+
+static doom_key_t doom_key_from_scancode(uint8_t scancode)
+{
+    switch (scancode) {
+    case 0x01:
+        return DOOM_KEY_ESCAPE;
+    case 0x0F:
+        return DOOM_KEY_TAB;
+    case 0x1C:
+        return DOOM_KEY_ENTER;
+    case 0x39:
+        return DOOM_KEY_SPACE;
+    case 0x0E:
+        return DOOM_KEY_BACKSPACE;
+
+    case 0x02:
+        return DOOM_KEY_1;
+    case 0x03:
+        return DOOM_KEY_2;
+    case 0x04:
+        return DOOM_KEY_3;
+    case 0x05:
+        return DOOM_KEY_4;
+    case 0x06:
+        return DOOM_KEY_5;
+    case 0x07:
+        return DOOM_KEY_6;
+    case 0x08:
+        return DOOM_KEY_7;
+    case 0x09:
+        return DOOM_KEY_8;
+    case 0x0A:
+        return DOOM_KEY_9;
+    case 0x0B:
+        return DOOM_KEY_0;
+
+    case 0x10:
+        return DOOM_KEY_Q;
+    case 0x11:
+        return DOOM_KEY_W;
+    case 0x12:
+        return DOOM_KEY_E;
+    case 0x13:
+        return DOOM_KEY_R;
+    case 0x14:
+        return DOOM_KEY_T;
+    case 0x15:
+        return DOOM_KEY_Y;
+    case 0x16:
+        return DOOM_KEY_U;
+    case 0x17:
+        return DOOM_KEY_I;
+    case 0x18:
+        return DOOM_KEY_O;
+    case 0x19:
+        return DOOM_KEY_P;
+
+    case 0x1E:
+        return DOOM_KEY_A;
+    case 0x1F:
+        return DOOM_KEY_S;
+    case 0x20:
+        return DOOM_KEY_D;
+    case 0x21:
+        return DOOM_KEY_F;
+    case 0x22:
+        return DOOM_KEY_G;
+    case 0x23:
+        return DOOM_KEY_H;
+    case 0x24:
+        return DOOM_KEY_J;
+    case 0x25:
+        return DOOM_KEY_K;
+    case 0x26:
+        return DOOM_KEY_L;
+
+    case 0x2C:
+        return DOOM_KEY_Z;
+    case 0x2D:
+        return DOOM_KEY_X;
+    case 0x2E:
+        return DOOM_KEY_C;
+    case 0x2F:
+        return DOOM_KEY_V;
+    case 0x30:
+        return DOOM_KEY_B;
+    case 0x31:
+        return DOOM_KEY_N;
+    case 0x32:
+        return DOOM_KEY_M;
+
+    case 0x33:
+        return DOOM_KEY_COMMA;
+    case 0x34:
+        return DOOM_KEY_PERIOD;
+    case 0x35:
+        return DOOM_KEY_SLASH;
+    case 0x27:
+        return DOOM_KEY_SEMICOLON;
+    case 0x0C:
+        return DOOM_KEY_MINUS;
+    case 0x0D:
+        return DOOM_KEY_EQUALS;
+    case 0x1A:
+        return DOOM_KEY_LEFT_BRACKET;
+    case 0x1B:
+        return DOOM_KEY_RIGHT_BRACKET;
+    case 0x28:
+        return DOOM_KEY_APOSTROPHE;
+
+    case 0x1D:
+        return DOOM_KEY_CTRL;
+    case 0x2A:
+    case 0x36:
+        return DOOM_KEY_SHIFT;
+    case 0x38:
+        return DOOM_KEY_ALT;
+
+    case 0x3B:
+        return DOOM_KEY_F1;
+    case 0x3C:
+        return DOOM_KEY_F2;
+    case 0x3D:
+        return DOOM_KEY_F3;
+    case 0x3E:
+        return DOOM_KEY_F4;
+    case 0x3F:
+        return DOOM_KEY_F5;
+    case 0x40:
+        return DOOM_KEY_F6;
+    case 0x41:
+        return DOOM_KEY_F7;
+    case 0x42:
+        return DOOM_KEY_F8;
+    case 0x43:
+        return DOOM_KEY_F9;
+    case 0x44:
+        return DOOM_KEY_F10;
+    case 0x57:
+        return DOOM_KEY_F11;
+    case 0x58:
+        return DOOM_KEY_F12;
+
+    case 0x48:
+        return DOOM_KEY_UP_ARROW;
+    case 0x50:
+        return DOOM_KEY_DOWN_ARROW;
+    case 0x4B:
+        return DOOM_KEY_LEFT_ARROW;
+    case 0x4D:
+        return DOOM_KEY_RIGHT_ARROW;
+
+    default:
+        return DOOM_KEY_UNKNOWN;
+    }
+}
+
+enum {
+    KEYBOARD_HOLD = 0,
+    KEYBOARD_PRESSED = 1,
+    KEYBOARD_RELEASED = 2,
+};
+
+static void handle_doom_keyboard_event(const window_keyboard_event_t *event)
+{
+    if (!event)
+        return;
+
+    doom_key_t key = doom_key_from_scancode(event->keyboard.scancode);
+    if (key == DOOM_KEY_UNKNOWN)
+        return;
+
+    if (event->keyboard.action == KEYBOARD_RELEASED)
+        doom_key_up(key);
+    else if (event->keyboard.action == KEYBOARD_PRESSED || event->keyboard.action == KEYBOARD_HOLD)
+        doom_key_down(key);
+}
+
+
+
+static void blit_doom_frame(gfx_context_t *ctx, uint32_t win_w, uint32_t win_h)
+{
+    if (!ctx || !ctx->backbuffer)
+        return;
+
+    const uint32_t src_w = 320;
+    const uint32_t src_h = 200;
+    const unsigned char *src = doom_get_framebuffer(4);
+    if (!src)
+        return;
+
+    uint32_t dst_w = win_w;
+    uint32_t dst_h = win_h;
+    if (dst_w == 0 || dst_h == 0)
+        return;
+
+    uint32_t *dst = (uint32_t *) ctx->backbuffer;
+    uint32_t dst_pitch = (uint32_t) ctx->width;
+
+    for (uint32_t y = 0; y < dst_h; y++) {
+        uint32_t src_y = (uint32_t) ((uint64_t) y * src_h / dst_h);
+        const unsigned char *src_row = src + src_y * src_w * 4;
+        uint32_t *dst_row = dst + y * dst_pitch;
+
+        for (uint32_t x = 0; x < dst_w; x++) {
+            uint32_t src_x = (uint32_t) ((uint64_t) x * src_w / dst_w);
+            const unsigned char *pixel = src_row + src_x * 4;
+            uint8_t r = pixel[0];
+            uint8_t g = pixel[1];
+            uint8_t b = pixel[2];
+            uint8_t a = 0xFF;
+            dst_row[x] = ((uint32_t) a << 24) | ((uint32_t) r << 16)
+                         | ((uint32_t) g << 8) | (uint32_t) b;
+        }
+    }
+}
+
+int main(void)
+{
+    doom_set_print(doom_print_override);
+    doom_set_malloc(doom_malloc_override, doom_free_override);
+    doom_set_file_io(
+        doom_open_override,
+        doom_close_override,
+        doom_read_override,
+        doom_write_override,
+        doom_seek_override,
+        doom_tell_override,
+        doom_eof_override);
+    doom_set_gettime(doom_gettime_override);
+    doom_set_exit(doom_exit_override);
+    doom_set_getenv(doom_getenv_override);
+
+    char *argv[32] = {"system:/doom", "system:/assets/doom1.wad"};
+    doom_init(
+        2,
+        argv,
+        DOOM_FLAG_MENU_DARKEN_BG | DOOM_FLAG_HIDE_MUSIC_OPTIONS | DOOM_FLAG_HIDE_SOUND_OPTIONS);
+
+    bool created = false;
+    bool create_pending = false;
+    bool framebuffer_ready = false;
+
+    uint32_t create_sequence = 0;
+    uint32_t window_id = 0;
+    uint32_t width = 0;
+    uint32_t height = 0;
+
+    gfx_context_t framebuffer = {0};
+
+    while (1) {
+        if (!created && !create_pending) {
+            create_sequence = desktop_create_window(
+                640,
+                480,
+                (window_flags_t) {
+                    .borderless = false,
+                    .fullscreen = false,
+                    .resizable = false,
+                    .maximized = false,
+                    .minimized = false,
+                },
+                "DOOM");
+            create_pending = true;
+        }
+
+        desktop_event_t event;
+        int poll_result = desktop_poll_event(&event);
+
+        if (poll_result == 0) {
+            if (event.type == DESKTOP_EVENT_WINDOW_CREATED && create_pending
+                && event.sequence == create_sequence) {
+                if (event.data.created.status != WINDOW_CREATED_SUCCESS) {
+                    create_pending = false;
+                    usleep(1);
+                    continue;
+                }
+
+                created = true;
+                create_pending = false;
+                framebuffer_ready = false;
+                window_id = event.data.created.id;
+                width = event.data.created.width;
+                height = event.data.created.height;
+
+                gfx_context_res_t framebuffer_res
+                    = desktop_request_window_framebuffer(window_id, width, height);
+                framebuffer = framebuffer_res.context;
+                if (desktop_get_error() == DESKTOP_ERROR_NONE) {
+                    gfx_set_target_fps(&framebuffer, 60);
+                    framebuffer_ready = true;
+                }
+
+                continue;
+            }
+
+            if (event.type == DESKTOP_EVENT_FRAMEBUFFER_READY
+                && event.data.framebuffer_ready.id == window_id
+                && desktop_handle_framebuffer_event(&event, &framebuffer) == 1) {
+                gfx_set_target_fps(&framebuffer, 60);
+                framebuffer_ready = true;
+                continue;
+            }
+
+            if (event.type == DESKTOP_EVENT_WINDOW_RESIZED && event.data.resized.id == window_id) {
+                width = event.data.resized.new_width;
+                height = event.data.resized.new_height;
+
+                gfx_context_res_t framebuffer_res
+                    = desktop_request_window_framebuffer(window_id, width, height);
+                framebuffer = framebuffer_res.context;
+                if (desktop_get_error() == DESKTOP_ERROR_NONE) {
+                    gfx_set_target_fps(&framebuffer, 60);
+                    framebuffer_ready = true;
+                } else {
+                    framebuffer_ready = false;
+                }
+                continue;
+            }
+
+            if (event.type == DESKTOP_EVENT_WINDOW_KEYBOARD && event.data.keyboard.id == window_id) {
+                handle_doom_keyboard_event(&event.data.keyboard);
+                continue;
+            }
+
+            if ((event.type == DESKTOP_EVENT_WINDOW_CLOSE && event.data.close.id == window_id)
+                || (event.type == DESKTOP_EVENT_WINDOW_CLOSED
+                    && event.data.closed.id == window_id)) {
+                break;
+            }
+
+            if (event.type == DESKTOP_EVENT_ERROR) {
+                if (created && !framebuffer_ready) {
+                    gfx_context_res_t framebuffer_res
+                        = desktop_request_window_framebuffer(window_id, width, height);
+                    framebuffer = framebuffer_res.context;
+                    if (desktop_get_error() == DESKTOP_ERROR_NONE) {
+                        gfx_set_target_fps(&framebuffer, 60);
+                        framebuffer_ready = true;
+                    }
+                }
+            }
+        }
+
+        if (created && framebuffer_ready) {
+            doom_update();
+
+            gfx_begin_frame(&framebuffer);
+            blit_doom_frame(&framebuffer, width, height);
+            gfx_end_frame(&framebuffer);
+
+            desktop_present_window((uint16_t) window_id);
+        } else {
+            usleep(1);
+        }
+    }
+
+    exit();
+}
